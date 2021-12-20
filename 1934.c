767c9d2348f66e4e255668658d3701f0ebbb95c1
seungbum_linux <tjtmdqja1727@gmail.com>
2021-12-20 04:03:38 +0000

    1934.c

---
+++ 1934.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int getNumber(int a, int b) {
+	if(b==0) return a;
+	else {
+		getNumber(b,a%b);
+	}
+}
+
+
+int main() {
+	
+	int t;
+	int a,b;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++) {
+		scanf("%d %d",&a,&b);
+		printf("%d\n",(a*b)/getNumber(a,b));
+
+	}
+
+
+	return 0;
+}
+
