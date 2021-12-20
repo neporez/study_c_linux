aaed079a2d74f944d9fa8b216ee026f41f03e433
seungbum_linux <tjtmdqja1727@gmail.com>
2021-12-20 03:46:01 +0000

    2775.c edit

--- 2775.c
+++ 2775.c
@@ -4,7 +4,7 @@ long long int apart[15][15];
 
 long long int livePeople(int i, int j) {
 	long long int sum = 0;
-	for(int k=0;k<=j;k++) {
+	for(int k=1;k<=j;k++) {
 		sum+=apart[i-1][k];
 	}	
 	return sum;
@@ -15,12 +15,12 @@ int main() {
 	int k,n;
 	scanf("%d",&t);
 
-	for(int i=0;i<15;i++) {
+	for(int i=1;i<15;i++) {
 		apart[0][i]=i;
 	}
 	
 	for(int i=1;i<15;i++) {
-		for(int j=0;j<15;j++) {
+		for(int j=1;j<15;j++) {
 			apart[i][j] = livePeople(i,j);
 		}
 	}
