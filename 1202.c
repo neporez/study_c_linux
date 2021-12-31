3470f805d64a39f242c63110fdca0272a1d76eef
neporez <tjtmdqja1727@gmail.com>
2021-12-31 03:10:37 +0000

    1202.c

--- 1202.c
+++ 1202.c
@@ -1,91 +1,191 @@
 #include<stdio.h>
 
-#define SWAP(x,y,temp) (temp=x,x=y,y=temp)
-
+int bag[300001];
 
 typedef struct jewel {
 	int value;
 	int weight;
 }JEWEL;
 
-int bag[300000];
-
-JEWEL jewel[300000];
-JEWEL pq[300000];
-
-int partitionJ(JEWEL list[],int left, int right) {
-	int low,high,pivot,temp;
+JEWEL jewel[300001];
+JEWEL jewel_heap[300001];
 
 
-
-
+void addJewel(int weight, int value, int index) {
+	JEWEL temp;
+	temp.weight = weight;
+	temp.value = value;
+	jewel[index] = temp;
+	
+	while((jewel[index/2].weight > jewel[index].weight) && index != 1) {
+		temp = jewel[index/2];
+		jewel[index/2] = jewel[index];
+		jewel[index] = temp;
+		index/=2;
+	}
+	
 }
+void addBag(int weight, int index) {
+	int temp;
+	temp = weight;
+	bag[index] = temp;
 
-
-void qj_sort(JEWEL list[], int left, int right) {
-	if(left < right) {
-		int q = partitionJ(list,left,right);
-
-		qj_sort(list,left,q-1);
-		qj_sort(list,q+1,right);
+	while((bag[index/2] > bag[index]) && index!=1) {
+		temp = bag[index/2];
+		bag[index/2] = bag[index];
+		bag[index] = temp;
+		index/=2;
 	}
 }
+int popBag(int index) {
+	int temp;
+	int returnTemp = bag[1];
+	int index_p = 1;
+	bag[1] = bag[index];
+	bag[index] = 0;
 
-int partition(int list[], int left, int right) {
-	int low,high,pivot,temp;
-	low = left;
-	high = right+1;
-	pivot = list[left];
-	
-	do {
-		do {
-
-			low++;
-		}while(pivot > list[low] && low<right);
-
-		do {
-			high--;
-		}while(pivot < list[high] && high > left);
-		if(low<high) {
-			SWAP(list[low],list[high],temp);
+	while(index_p*2 < index && (bag[index_p] > bag[index_p*2] || bag[index_p] > bag[index_p*2+1])) {
+		if(bag[index_p*2] !=0 && bag[index_p*2+1] !=0) {	
+			if(bag[index_p*2] < bag[index_p*2+1]) {
+				temp = bag[index_p*2];
+			       	bag[index_p*2] = bag[index_p];
+				bag[index_p] = temp;
+				index_p *=2;	
+			} else {
+				temp = bag[index_p*2+1];
+				bag[index_p*2+1] = bag[index_p];
+				bag[index_p] = temp;
+				index_p = index_p*2+1;
+			}
+		} else if(bag[index_p*2+1] == 0 && bag[index_p*2] < bag[index_p]) {
+				temp = bag[index_p*2];
+			       	bag[index_p*2] = bag[index_p];
+				bag[index_p] = temp;
+				index_p *=2;	
+		} else {
+			break;
 		}
-	}while(low<high);
-	
-	SWAP(list[left],list[high],temp);
-
-	return high;
+	}
+	return returnTemp;
 }
 
-void q_sort(int list[], int left, int right) {
-	if(left < right) {
-		int q = partition(list,left,right);
+void addHeap(int weight, int value, int index) {
+	JEWEL temp;
+	temp.weight = weight;
+	temp.value = value;
+	jewel_heap[index] = temp;
 
-		q_sort(list,0,q-1);
-		q_sort(list,q+1,right);
+	while(index!=1 &&(jewel_heap[index/2].value < jewel_heap[index].value)) {
+		temp = jewel_heap[index/2];
+		jewel_heap[index/2] = jewel_heap[index];
+		jewel_heap[index] = temp;
+		index/=2;
+	}
+}
+
+JEWEL popJewel(int index) {
+	JEWEL temp;
+	JEWEL returnTemp = jewel[1];
+	int index_p = 1;
+	jewel[1] = jewel[index];
+	jewel[index].weight = 0;
+	jewel[index].value = 0;
+	
+	while(index_p*2<index && (jewel[index_p].weight > jewel[index_p*2].weight || jewel[index_p].weight > jewel[index_p*2+1].weight)) {
+		if(jewel[index_p*2].weight != 0 && jewel[index_p*2+1].weight != 0) {
+			if(jewel[index_p*2].weight < jewel[index_p*2+1].weight) {
+				temp = jewel[index_p*2];
+				jewel[index_p*2] = jewel[index_p];
+				jewel[index_p] = temp;
+				index_p*=2;
+			} else {
+				temp = jewel[index_p*2+1];
+				jewel[index_p*2+1] = jewel[index_p];
+				jewel[index_p] = temp;
+				index_p = index_p*2+1;
+			}
+	       	} else if(jewel[index_p*2+1].weight == 0 && jewel[index_p*2].weight < jewel[index_p].weight) {
+			temp = jewel[index_p*2];
+			jewel[index_p*2] = jewel[index_p];
+			jewel[index_p] = temp;
+			index_p*=2;
+		} else {
+			break;
+		}
 
 	}
 
+
+	return returnTemp;
 }
 
+JEWEL popHeap(int index) {
+	JEWEL temp;
+	JEWEL returnTemp = jewel_heap[1];
+	int index_p = 1;
+	jewel_heap[1] = jewel_heap[index];
+	jewel_heap[index].weight = 0;
+	jewel_heap[index].value = 0;
+
+	while(index_p*2<index && (jewel_heap[index_p].value < jewel_heap[index_p*2].value || jewel_heap[index_p].value < jewel_heap[index_p*2+1].value)) {
+		if(jewel_heap[index_p*2].value != 0 && jewel_heap[index_p*2+1].value !=0) {	
+			if(jewel_heap[index_p*2].value > jewel_heap[index_p*2+1].value) {
+				temp = jewel_heap[index_p*2];
+				jewel_heap[index_p*2] = jewel_heap[index_p];
+				jewel_heap[index_p] = temp;
+				index_p*=2;
+			} else {
+				temp = jewel_heap[index_p*2+1];
+				jewel_heap[index_p*2+1] = jewel_heap[index_p];
+				jewel_heap[index_p] = temp;
+				index_p = index_p*2+1;
+			}
+		} else if(jewel_heap[index_p*2+1].value == 0 && jewel_heap[index_p*2].value > jewel_heap[index_p].value) {
+			temp = jewel_heap[index_p*2];
+			jewel_heap[index_p*2] = jewel_heap[index_p];
+			jewel_heap[index_p] = temp;
+			index_p*=2;
+		} else {
+			break;
+		}
+	}
+
+
+	return returnTemp;
+
+}
 
 int main() {
-	int n,k,temp,temp2;
+	int n,k,temp1,temp2,index_jewel = 1, index_bag=1, index_heap = 1;
+	int bagWeight,sum=0;
+	JEWEL tempJewel;
 	scanf("%d %d",&n,&k);
-
+	
 	for(int i=0;i<n;i++) {
-		scanf("%d %d",);
+		scanf("%d %d",&temp1,&temp2);
+		addJewel(temp1,temp2,index_jewel++);
 	}
-
 	for(int i=0;i<k;i++) {
-		scanf("%d",&bag[i]);
+		scanf("%d",&temp1);
+		addBag(temp1,index_bag++);
 	}
+	while(bag[1] > 0 && jewel[1].weight > 0) {
+		bagWeight = popBag(--index_bag);
+		while(bagWeight >= jewel[1].weight && index_jewel > 1) {
+			tempJewel = popJewel(--index_jewel);
+			addHeap(tempJewel.weight, tempJewel.value, index_heap++);
+			
+		}
+		if(jewel_heap[1].weight != 0) {
+			sum+=popHeap(--index_heap).value;
+		}
+		while(jewel_heap[1].weight != 0) {
+			tempJewel= popHeap(--index_heap);
+			addJewel(tempJewel.weight,tempJewel.value,index_jewel++);
+		}
+	}	
 
-	q_sort(bag,0,k-1);
-
-
-
-
-
+	printf("%d",sum);
 
 	return 0;
 }
